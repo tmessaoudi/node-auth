df0edffd99cd6b22452bb8ea8c78bef9c8b7738a
Tarek Messaoudi <tmessaoudi94@gmail.com>
2022-08-03 23:03:28 +0200

    Remove celebrate/joi and replace it with zod

--- api/src/validation.ts
+++ api/src/validation.ts
@@ -1,81 +1,74 @@
-import { celebrate, SchemaOptions, Modes, Segments, Joi } from "celebrate";
+import { z, ZodSchema } from "zod"
+import { RequestHandler } from "express"
 import { PWD_RESET_TOKEN_BYTES } from "./config/auth";
 
 // TODO Use https://zod.dev/ for validation
-export const validate = (schema: SchemaOptions) =>
-  celebrate(
-    schema,
-    {
-      abortEarly: false, // validate all fields in the segment
-    },
-    {
-      mode: Modes.FULL, // validate all segments (body, query, etc.)
-    }
-  );
+export const validate = (schema: ZodSchema): RequestHandler => (req, res, next) => {
+  try {
+    schema.parse({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    })
 
-const email = Joi.string().email().required();
+    next()
+  } catch (err) {
+    return res.status(400).send(err)
+  }
+}
+
+
+const email = z.string().email();
 
 // NOTE instead of prehashing passwords with SHA256, we could limit
 // them to 72 bytes (important: not characters) like so: .max(72, 'utf8')
 // However, this would likely leak our password algorithm (i.e. bcrypt).
-const password = Joi.string().max(256).required(); // TODO password strength
+const password = z.string().max(256) // TODO password strength
+const username = z.string().max(256) // TODO password strength
 
-export const loginSchema = {
-  [Segments.BODY]: Joi.object().keys({
+export const loginSchema = z.object({
+  body: z.object({
     email,
     password,
-  }),
-};
+  })
+})
 
-export const registerSchema = {
-  [Segments.BODY]: Joi.object().keys({
+export const registerSchema = z.object({
+  body: z.object({
     email,
     password,
-    username: Joi.string().max(256).required(),
-  }),
-};
+    username,
+  })
+})
 
 // Based on Postgres `serial` type (4 bytes, roughly 2.1B)
 // https://www.postgresql.org/docs/9.1/datatype-numeric.html
-const id = Joi.number()
-  .positive() // can't be zero or negative
-  .max(2 ** 31 - 1)
-  .required();
+const id = z.number().positive().max(2 ** 31 - 1) // can't be zero or negative
 
-export const verifyEmailSchema = {
-  [Segments.QUERY]: {
+export const verifyEmailSchema = z.object({
+  query: z.object({
     id,
-    expires: Joi.date().timestamp().raw().required(), // `raw` means it's not casted to a Date
-    signature: Joi.string().length(64).required(), // 256 / 8 * 2 (hex)
-  },
-};
-
-export const resendEmailSchema = {
-  [Segments.BODY]: Joi.object().keys({
-    email,
+    expires: z.date(), // `raw` means it's not casted to a Date
+    signature: z.string().length(64), // 256 / 8 * 2 (hex)
   }),
-};
+})
 
-export const sendResetSchema = {
-  [Segments.BODY]: Joi.object().keys({
-    email,
-  }),
-};
+export const resendEmailSchema = z.object({
+  body: z.object({ email })
+})
 
-export const resetPasswordSchema = {
-  [Segments.QUERY]: {
+export const sendResetSchema = z.object({
+  body: z.object({ email })
+})
+
+export const resetPasswordSchema = z.object({
+  query: z.object({
     id,
-    token: Joi.string()
-      .length(PWD_RESET_TOKEN_BYTES * 2) // hex
-      .required(),
-  },
-  [Segments.BODY]: Joi.object().keys({
-    password,
+    token: z.string().length(PWD_RESET_TOKEN_BYTES * 2) // hex
   }),
-};
+  body: z.object({ password }),
+})
 
-export const confirmPasswordSchema = {
-  [Segments.BODY]: Joi.object().keys({
-    password,
-  }),
-};
+export const confirmPasswordSchema = z.object({
+  body: z.object({ password })
+})
